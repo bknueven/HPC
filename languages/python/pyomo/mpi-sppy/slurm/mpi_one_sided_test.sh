b6e770c89eacd871613afafae35f1db61a0f1f47
Bernard Knueven <Bernard.Knueven@nrel.gov>
2023-11-13 14:48:40 -0700

    working mpi_one_sided_test runner

--- languages/python/pyomo/mpi-sppy/slurm/mpi_one_sided_test.sh
+++ languages/python/pyomo/mpi-sppy/slurm/mpi_one_sided_test.sh
@@ -2,18 +2,17 @@
 #SBATCH --nodes=2               # Number of nodes
 #SBATCH --ntasks=2              # Request 2 CPU cores
 #SBATCH --time=00:01:00         # Job should run for up to 1 minute
-#SBATCH --account=aces  	# Where to charge NREL Hours
+#SBATCH --account=aiuserapps    # Where to charge NREL Hours
 #SBATCH --ntasks-per-node=1     # Put a task on each node
-#SBATCH --mail-user=Firstname.Lastname@nrel.gov  # If you want email notifications
+#SBATCH --mail-user=Bernard.Knueven@nrel.gov  # If you want email notifications
 #SBATCH --mail-type=BEGIN,END,FAIL		 # When you want email notifications
 #SBATCH --output=mpisppy_test.%j.out  # %j will be replaced with the job ID
 
-module load conda
-
-conda activate pyomo              # activate your conda environment
+module load anaconda3
+conda activate mpi-sppy # activate your conda environment
 
 export MPICH_ASYNC_PROGRESS=1     # enable communication threads
 
-cd ${HOME}/software/mpi-sppy      # the location of mpi-sppy
+cd /projects/aiuserapps/bknueven/software/mpi-sppy      # the location of mpi-sppy
 
 srun python -u -m mpi4py mpi_one_sided_test.py
